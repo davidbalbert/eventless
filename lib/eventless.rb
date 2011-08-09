8c701065482cabddac90d31e3952a1414b2a635c
David Albert <davidbalbert@gmail.com>
2011-08-09 15:23:25 -0400

    single non-blocking socket!

--- lib/eventless.rb
+++ lib/eventless.rb
@@ -1,5 +1,44 @@
-require "eventless/version"
+#require 'eventless/version'
+require 'socket'
+require 'fcntl'
+
+STDIN.sync = STDOUT.sync = true
 
 module Eventless
-  # Your code goes here...
+  class Loop
+    def initialize
+      @read_fds,  @write_fds, @error_fds = [], [], []
+    end
+    def run
+      @running = true
+      while @running
+        IO::select(@read_fds, @write_fds, @error_fds)
+      end
+    end
+  end
 end
+
+class BasicSocket < IO
+
+  alias_method :recv_block, :recv
+
+  def recv(*args)
+    mesg = ""
+    begin
+      flags = fcntl(Fcntl::F_GETFL, 0)
+      mesg << recv_nonblock(*args)
+      fcntl(Fcntl::F_SETFL, flags)
+    rescue IO::WaitReadable
+      fcntl(Fcntl::F_SETFL, flags)
+      STDOUT.puts "about to select"
+      IO.select([self])
+      retry
+    end
+    mesg
+  end
+end
+
+serv = TCPServer.new("127.0.0.1", 12345)
+af, port, host, addr = serv.addr
+s = serv.accept
+puts s.recv(10)
