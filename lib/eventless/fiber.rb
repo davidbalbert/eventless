4a1eb1ac4bfd90b990589abc9bda7a0e8a69efff
David Albert <davidbalbert@gmail.com>
2011-10-11 13:51:48 -0400

    Monkey patch Fiber instead of subclassing it
    This is necessary for the root fiber to be able to support
    transfer_and_raise.

--- lib/eventless/fiber.rb
+++ lib/eventless/fiber.rb
@@ -1,71 +1,102 @@
 require 'fiber'
 
-module Eventless
-  class Fiber < ::Fiber
-    attr_reader :result, :exception
-    def initialize(parent=Fiber.current, &block)
-      @parent = parent if parent
-      @links = []
-      @exception = @result = nil
+class Fiber
+  attr_reader :result, :exception
 
-      # It seems without (), super passes all the args that we're
-      # called with
-      super() do
-        begin
-          @result = block.call
-        rescue StandardError => e
-          @exception = e
-          print_exception
-        end
+  alias_method :initialize_original, :initialize
+  def initialize(parent=Fiber.current, &block)
+    @links = []
+    exception = result = to_raise = nil
 
-        Eventless.loop.timer(0) do
-          @links.each { |obj, method| obj.send(method, self) }
-        end
+    initialize_original do
+      begin
+        # in case someone started us with transfer_and_raise
+        raise_after_transfer!
 
-        @dead = true
-        @parent.transfer if @parent
+        @result = block.call
+      rescue StandardError => e
+        Fiber.current[:dead] = true
+        @exception = e
+        print_exception
       end
-    end
 
-    def print_exception
-      puts "#{@exception.class}: #{@exception.message}", @exception.backtrace
-    end
+      Eventless.loop.timer(0) do
+        @links.each { |obj, method| obj.send(method, self) }
+      end
 
-    def transfer(*args)
-      raise FiberError, "dead fiber called" if @dead
-      super(*args)
+      Fiber.current[:dead] = true
+      parent.transfer if parent
     end
+  end
 
-    def alive?
-      return false if @dead
-      super
+  def print_exception
+    STDERR.puts "#{@exception.class}: #{@exception.message}", @exception.backtrace
+  end
+
+  alias_method :transfer_original, :transfer
+  def transfer(*args)
+    raise FiberError, "dead fiber called" if self[:dead]
+    transfer_original(*args) # doesn't return until we get transfered back
+
+    raise_after_transfer!
+  end
+
+  def transfer_and_raise(exception)
+    self[:to_raise] = exception
+    transfer
+  end
+
+  def raise_after_transfer!
+    if Fiber.current[:to_raise]
+      ex = Fiber.current[:to_raise]
+      ex = ex.call if ex.respond_to? :call # for testing
+      Fiber.current[:to_raise] = nil
+      raise ex
     end
+  end
 
-    def dead?
-      not alive?
-    end
+  alias_method :alive_original?, :alive?
+  def alive?
+    return false if self[:dead]
+    alive_original?
+  end
 
-    def success?
-      @exception.nil?
-    end
+  def dead?
+    not alive?
+  end
 
-    def join
-      return if dead?
+  def success?
+    @exception.nil?
+  end
 
-      # XXX: Will need to implement unlink to handle exceptions
-      current = Fiber.current
-      link(Fiber.current, :transfer)
-      Eventless.loop.transfer
-    end
+  def join
+    return if dead?
 
-    def link(obj, method)
-      @links << [obj, method]
-      # XXX: should make this check if the fiber is already dead and then
-      # schedule immediately
-    end
+    # XXX: Will need to implement unlink to handle exceptions
+    current = Fiber.current
+    link(Fiber.current, :transfer)
+    Eventless.loop.transfer
+  end
 
-    def unlink(obj, method)
-      @links.remove([obj, method])
-    end
+  def link(obj, method)
+    @links << [obj, method]
+    # XXX: should make this check if the fiber is already dead and then
+    # schedule immediately
+  end
+
+  def unlink(obj, method)
+    @links.remove([obj, method])
+  end
+
+  def [](key)
+    fiber_vars[key]
+  end
+
+  def []=(key, val)
+    fiber_vars[key] = val
+  end
+
+  def fiber_vars
+    @fiber_vars ||= {}
   end
 end
