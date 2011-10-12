91ea7e8878629cd02b90cbce1f9531947ef8d461
David Albert <davidbalbert@gmail.com>
2011-10-12 01:55:32 -0400

    add not working examples for Event and open-uri

---
+++ examples/event.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+$:.unshift File.join(File.dirname(__FILE__), "..", "lib")
+
+require 'eventless'
+
+event = Eventless::Event.new
+
+waiters = []
+5.times do |i|
+  waiters << Eventless.spawn do
+    event.wait
+    puts "I am number #{i} and I'm awake!"
+  end
+end
+
+sleep 1 # let the waiters all start waiting
+#event.set!
+
+waiters.each { |waiter| waiter.join }
