d9c1a28df67e25e43570e8d31de425386e6d2482
David Albert <davidbalbert@gmail.com>
2011-10-18 18:47:24 -0400

    working IO.select (I think)

--- lib/eventless/select.rb
+++ lib/eventless/select.rb
@@ -21,17 +21,27 @@ module Eventless
     watchers = []
     ready = SelectResults.new
 
-    read_array.each do |io|
-      watcher = Eventless.loop.io(:read, io) { ready.append_read(io) }
-      Eventless.loop.attach(watcher)
-      watchers << watcher
+    write_array = [] if write_array.nil?
+
+    begin
+      read_array.each do |io|
+        watcher = Eventless.loop.io(:read, io) { ready.append_read(io) }
+        Eventless.loop.attach(watcher)
+        watchers << watcher
+      end
+
+      write_array.each do |io|
+        watcher = Eventless.loop.io(:write, io) { ready.append_write(io) }
+        Eventless.loop.attach(watcher)
+        watchers << watcher
+      end
+
+      ready.event.wait(timeout)
+      return ready.to_read, ready.to_write, []
+    ensure
+      watchers.each { |w| w.detach }
     end
 
-    write_array.each do |io|
-      watcher = Eventless.loop.io(:write, io) { ready.append_write(io) }
-      Eventless.loop.attach(watcher)
-      watchers << watcher
-    end
   end
 
   class SelectResults
@@ -45,12 +55,12 @@ module Eventless
 
     def append_read(io)
       @to_read << io
-      @event.set
+      @event.set!
     end
 
     def append_write(io)
       @to_write << io
-      @event.set
+      @event.set!
     end
   end
 end
