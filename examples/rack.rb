58c82ce0897a9a5e7477e4e9476b8949f21a5c5f
David Albert <davidbalbert@gmail.com>
2011-10-21 03:12:56 -0400

    add demo rack app

---
+++ examples/rack.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+#
+
+$:.unshift File.join(File.dirname(__FILE__), "..", "lib")
+
+require 'rack'
+#require 'eventless'
+
+#require 'socket'
+#require 'kgb'
+
+#KGB.spy_on(Socket, TCPSocket, TCPServer)
+
+Rack::Server.start(
+  :app => lambda do |e|
+    [200, {'Content-Type' => 'text/html'}, ['hello world']]
+  end,
+  :Port => 3000,
+)
