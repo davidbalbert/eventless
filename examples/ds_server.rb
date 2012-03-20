fbd1601e88617dc4064c59d6a68c3718408e31a1
David Albert <davidbalbert@gmail.com>
2012-03-20 19:41:45 -0400

    added Danielle's client/server examples

---
+++ examples/ds_server.rb
@@ -0,0 +1,25 @@
+# Use in conjunction with ds_client.rb for testing puts/putc/gets/getc
+
+require 'bundler/setup'
+require 'socket'
+require 'eventless'
+
+server = TCPServer.new 2000
+
+loop {
+  while client = server.accept
+      client.puts "One"
+      client.puts "Hello!"
+      client.puts "Nice to see you!\n"
+      client.puts "Today would be improved by snails, don't you think?"
+      client.putc "Two"
+
+      tests = %w(three four five six seven eight nine ten).map do |word|
+        Eventless.spawn { sleep Random.rand(6); client.puts word }
+      end
+
+      tests.each { |t| t.join }
+
+      client.close
+  end
+}
